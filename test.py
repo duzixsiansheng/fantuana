d675c54cc3c0168d44d137acd48a7fc3c4bc1ff7
duzixsiansheng <stevey960324@gmail.com>
2022-04-25 18:07:44 -0400

    debug

--- test.py
+++ test.py
@@ -16,8 +16,8 @@ df = pd.read_csv('fantuan.csv')
 form = st.form(key="annotation",clear_on_submit = True)
 with form:
     cols = st.columns((2))
-    user_name = cols[0].text_input('User name')
-    user_ID = cols[1].text_input('User ID')
+    user_name = cols[0].text_input('微信名')
+    user_ID = cols[1].text_input('微信号')
     cols = st.columns((4))
     A = cols[0].number_input('汉堡贴纸',min_value=-100,max_value=1000,step=1,value=0)
     B = cols[1].number_input('甜品贴纸',min_value=-100,max_value=1000,step=1,value=0)
@@ -37,7 +37,7 @@ with form:
     cols = st.columns((4))
     M = cols[0].number_input('小吃贴纸',min_value=-100,max_value=1000,step=1,value=0)
 
-    addrow = {'user_name':user_name,'user_ID':user_ID,'汉堡贴纸':A,'甜品贴纸':B,'奶茶贴纸':C,
+    addrow = {'微信名':user_name,'微信号':user_ID,'汉堡贴纸':A,'甜品贴纸':B,'奶茶贴纸':C,
                 '韩餐日料贴纸':D,'川菜贴纸':E,'粤式早茶':F,'地方特色贴纸':G,'火锅贴纸':H
                 ,'烤串贴纸':I,'香锅麻辣烫贴纸':J,'面粉贴纸':K,'异域风味贴纸':L,'小吃贴纸':M}
     submitted = st.form_submit_button(label="submit!")
@@ -76,11 +76,11 @@ st.download_button(label=' Download Current Result',
                                 data=dl ,
                                 file_name= 'label.csv')
 
-grab = st.text_input('User name')
+grab = st.text_input('微信名')
 if st.button('get'):
-    nameList = df['user_name'].tolist()
+    nameList = df['微信名'].tolist()
     if grab in nameList:
-       gindex=df[df['user_name']==grab].index.values.astype(int)[0] 
+       gindex=df[df['微信名']==grab].index.values.astype(int)[0] 
        grab_df = df.loc[[gindex]]
        st.write(grab_df)
     else:
@@ -98,9 +98,9 @@ if st.button('check'):
             name_price.append(tmp[0])
     st.write(name_price)
 
-delete_name = st.text_input('delete ID')
+delete_name = st.text_input('delete 微信号')
 if st.button('Delete That Line!'):
-    if (delete_name in df['user_ID'].tolist()):
+    if (delete_name in df['微信号'].tolist()):
         st.text('deleting...')
-        df.drop(df[df['user_ID'] == delete_name].index,inplace=True)
+        df.drop(df[df['微信号'] == delete_name].index,inplace=True)
         df.to_csv('fantuan.csv', index=False)
